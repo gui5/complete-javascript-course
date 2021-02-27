ea5b695cb6e132c5891f7405d5e0f8932f5e5cfb
Guilherme de Oliveira <oliveira.gui5@gmail.com>
2021-02-27 01:58:11 -0300

    assigment 1

---
+++ 01-Fundamentals-Part-1/starter/script.js
@@ -0,0 +1,16 @@
+let js = 'amazing';
+console.log(40 + 80 + 23 - 10);
+console.log('Jonas');
+console.log(23);
+
+let firstName = "Matilda";
+console.log(firstName);
+console.log(firstName);
+console.log(firstName);
+console.log(firstName);
+let PI = 3.1415;
+
+let myFirstJob = "Programmer";
+let myCurrentJob = "Teacher";
+
+console.log(myFirstJob);
---
+++ assignments/fundamentals/values-and-variables/script.js
@@ -0,0 +1,7 @@
+let country = 'Brazil';
+let continent = 'South America';
+let populantion = 250;
+
+console.log(country);
+console.log(continent);
+console.log(populantion + " [mi]");
